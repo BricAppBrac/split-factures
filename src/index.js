732733ad6c1c1ac9c05d6d0769adf78ef22887e0
BricAppBrac <sandrinesteiger3000@gmail.com>
2024-01-13 10:18:53 +0100

    V1.0 avec désactivation eslint pour worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.scss";
+// eslint-disable-next-line no-unused-vars
 import pdfjs from "pdfjs-dist";
+// eslint-disable-next-line no-unused-vars
 import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
